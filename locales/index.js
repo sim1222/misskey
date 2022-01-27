c363154e07ece2d44c988ddce8193c67155def68
nullnyat <nullnyat@outlook.com>
2022-01-28 04:34:51 +0900

    治らんかったから戻してen-US追加した

--- locales/index.js
+++ locales/index.js
@@ -14,7 +14,7 @@ const merge = (...args) => args.reduce((a, c) => ({
 }), {});
 
 const languages = [
-	'ja-NCAT',
+	'ja-JP',
 ];
 
 const primaries = {
@@ -30,10 +30,12 @@ module.exports = Object.entries(locales)
 	.reduce((a, [k ,v]) => (a[k] = (() => {
 		const [lang] = k.split('-');
 		switch (k) {
-			case 'ja-NCAT': return v;
+			case 'ja-JP': return v;
+			case 'ja-KS':
+			case 'en-US': return merge(locales['ja-JP'], v);
 			default: return merge(
-				locales['ja-NCAT'],
-
+				locales['ja-JP'],
+				locales['en-US'],
 				locales[`${lang}-${primaries[lang]}`] || {},
 				v
 			);
