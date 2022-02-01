b08c243ce8c18413b02cbd9bb0bb23630c8f060b
こけっち <50144466+sim1222@users.noreply.github.com>
2022-02-01 13:12:56 +0900

    もじゃじゃじゃじゃどじゃwfびおえhっふぇsぶじあふぇ

--- packages/client/src/components/global/header.vue
+++ packages/client/src/components/global/header.vue
@@ -25,7 +25,7 @@
 		</div>
 	</template>
 	<div class="buttons right">
-		<button><i class="fas fa-redo"></i></button>
+		<button v-tooltip="Reload" class="_button button" @click.stop="location.reload()"><i class="fas fa-redo"></i></button>
 		<template v-if="info && info.actions && !narrow">
 			<template v-for="action in info.actions">
 				<MkButton v-if="action.asFullButton" class="fullButton" primary @click.stop="action.handler"><i :class="action.icon" style="margin-right: 6px;"></i>{{ action.text }}</MkButton>
