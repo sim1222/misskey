08d0b0f5ef743011332d40095271b998c09dfcfb
nullnyat <nullnyat@outlook.com>
2022-02-08 17:19:08 +0900

    ウィジェットの色

--- packages/client/src/widgets/server-metric/pie.vue
+++ packages/client/src/widgets/server-metric/pie.vue
@@ -14,7 +14,7 @@
 		:stroke-dashoffset="strokeDashoffset"
 		fill="none"
 		stroke-width="0.1"
-		stroke="#96CCE7"
+		stroke="#CECEFF"
 	/>
 	<text x="50%" y="50%" dy="0.05" text-anchor="middle">{{ (value * 100).toFixed(0) }}%</text>
 </svg>
