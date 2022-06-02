ad92b26a584e18c478cc2d8bf097045c8c4514d5
melt_adzuki <rebanira02@gmail.com>
2022-06-02 20:18:57 +0900

    refactor quick menu

--- packages/client/src/components/quick-menu.vue
+++ packages/client/src/components/quick-menu.vue
@@ -1,11 +1,10 @@
 <template>
 <MkModal ref="modal" v-slot="{ type, maxHeight }" :prefer-type="preferedModalType" :anchor="anchor" :transparent-bg="true" :src="src" @click="modal.close()" @closed="emit('closed')">
 	<div class="szkkfdyq _popup _shadow" :class="{ asDrawer: type === 'drawer' }" :style="{ maxHeight: maxHeight ? maxHeight + 'px' : '' }">
-		<div v-if="!(antennaItems.length <= 1) || !(listItems.length <= 1)" class="ghredhe">
+		<div v-if="antennaItems.length || listItems.length" class="ghredhe">
 			<div class="column">
 				<MkMenu :items="antennaItems" @click="close"/>
 			</div>
-
 			<div class="column">
 				<MkMenu :items="listItems" @click="close"/>
 			</div>
@@ -32,14 +31,14 @@
 <script lang="ts" setup>
 import * as os from '@/os';
 import { i18n } from '@/i18n';
-import { Ref, ref } from 'vue';
+import { onMounted, Ref, ref } from 'vue';
 import { quickAccessDef } from '@/menu';
-import { defaultStore } from '@/store';
 import { deviceKind } from '@/scripts/device-kind';
 import { MenuItem } from '@/types/menu';
 import MkModal from '@/components/ui/modal.vue';
 import MkMenu from '@/components/ui/menu.vue';
 import { $i, getAccounts, login } from '@/account';
+import { UserDetailed } from 'misskey-js/built/entities';
 
 const props = withDefaults(defineProps<{
 	src?: HTMLElement;
@@ -58,9 +57,7 @@ const preferedModalType = (deviceKind === 'desktop' && props.src != null) ? 'pop
 
 const modal = $ref<InstanceType<typeof MkModal>>();
 
-const menu = defaultStore.state.menu;
-
-const quickAccessItems = Object.keys(quickAccessDef).filter(k => !menu.includes(k)).map(k => quickAccessDef[k]).filter(def => def.show == null ? true : def.show).map(def => ({
+const quickAccessItems = Object.keys(quickAccessDef).map(k => quickAccessDef[k]).filter(def => def.show == null ? true : def.show).map(def => ({
 	type: def.to ? 'link' : 'button',
 	icon: def.icon,
 	to: def.to,
@@ -68,49 +65,69 @@ const quickAccessItems = Object.keys(quickAccessDef).filter(k => !menu.includes(
 	indicate: def.indicated,
 }));
 
-const accountItems = ref()
-getAccounts()
-	.then(accounts => accounts.filter(x => x.id !== $i?.id))
-	.then(storedAccounts => {
-		os.api('users/show', {
-			userIds: storedAccounts.map(x => x.id)
-		}).then(accounts => accountItems.value = accounts)
-	})
-
-const switchAccount = async account => {
-	const storedAccounts = await getAccounts()
-	const token = storedAccounts.find(x => x.id === account.id)?.token
-	login(token)
-}
-
 const antennaItems: Ref<MenuItem[]> = ref([])
 const listItems: Ref<MenuItem[]> = ref([])
+const accountItems: Ref<UserDetailed[]> = ref([])
 
-os.api('antennas/list')
-	.then(antennas => antennas.map(antenna => ({
+onMounted(async () => {
+	antennaItems.value = await getItemsOfAntennas()
+	listItems.value = await getItemsOfLists()
+	accountItems.value = await getItemsOfAccounts()
+})
+
+const getItemsOfAntennas = async (): Promise<MenuItem[]> => {
+	const antennas = await os.api('antennas/list')
+	if (!antennas.length) return []
+
+	const items = antennas.map(antenna => ({
 		type: 'link' as const,
 		text: antenna.name,
 		indicate: antenna.hasUnreadNote,
 		to: `/timeline/antenna/${antenna.id}`,
-	})))
-	.then(items => antennaItems.value = [{
-		type: "label" as const,
-		text: i18n.ts.antennas,
-	}, ...items])
+	}))
 
-os.api('users/lists/list')
-	.then(lists => lists.map(list => ({
+	return [{
+		type: 'label' as const,
+		text: i18n.ts.antennas,
+	}, ...items]
+}
+
+const getItemsOfLists = async (): Promise<MenuItem[]> => {
+	const lists = await os.api('users/lists/list')
+	if (!lists.length) return []
+
+	const items = lists.map(list => ({
 		type: 'link' as const,
 		text: list.name,
 		to: `/timeline/list/${list.id}`,
-	})))
-	.then(items => listItems.value = [{
-		type: "label" as const,
-		text: i18n.ts.lists,
-	}, ...items])
+	}))
 
-function close() {
-	modal.close();
+	return [{
+		type: 'label' as const,
+		text: i18n.ts.lists,
+	}, ...items]
+}
+
+const getItemsOfAccounts = async (): Promise<UserDetailed[]> => {
+	const accounts = await getAccounts()
+	const accountsWithoutCurrentUser = accounts.filter(account => account.id !== $i?.id)
+
+	const items = await os.api('users/show', {
+		userIds: accountsWithoutCurrentUser.map(account => account.id)
+	})
+
+	return items
+}
+
+const switchAccount = async (account): Promise<void> => {
+	const storedAccounts = await getAccounts()
+	const token = storedAccounts.find(x => x.id === account.id)?.token
+	
+	login(token)
+}
+
+const close = (): void => {
+	modal.close()
 }
 </script>
 
