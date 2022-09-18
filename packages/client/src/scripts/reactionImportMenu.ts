d06e9e7e0c07e6738f29fddc78469503d1bed02f
こけっち <kokt@sim1222.com>
2022-09-18 15:34:08 +0900

    wqip

--- packages/client/src/scripts/reactionImportMenu.ts
+++ packages/client/src/scripts/reactionImportMenu.ts
@@ -61,7 +61,7 @@ export async function openReactionImportMenu(ev: MouseEvent, reaction: string) {
 
 	const importEmoji = async (emojiName: string) => {
 		const emojiId = await getEmojiId(emojiName);
-		if (!emojiId) return;
+		if (!await emojiId) return;
 		os.api('admin/emoji/copy', {
 			emojiId: emojiId,
 		}).then(async emoji => os.popup(defineAsyncComponent(() => import('@/pages/admin/emoji-edit-dialog.vue')), {
